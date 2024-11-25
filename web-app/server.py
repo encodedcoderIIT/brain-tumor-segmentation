2b478974a4598823f0a3ed2d1c41655f7ce3fd6a
Suresh <sureshrao100000@gmail.com>
2024-11-26 02:18:17 +0530

    Update the header footer with common base.html, update team.html

--- web-app/server.py
+++ web-app/server.py
@@ -1,59 +1,3 @@
-# from flask import Flask, request, jsonify, render_template
-# import os
-# import gzip
-# import nibabel as nib
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from io import BytesIO
-# import base64
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/team')
-# def team():
-#     return render_template('team.html')
-
-# @app.route('/contact')
-# def contact():
-#     return render_template('contact.html')
-
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
-
-# @app.route('/upload', methods=['POST'])
-# def upload_files():
-#     files = request.files.getlist('files')
-#     images = []
-
-#     for file in files:
-#         if file.filename.endswith('.gz'):
-#             with gzip.open(file, 'rb') as f_in:
-#                 nii_file = nib.Nifti1Image.from_bytes(f_in.read())
-#                 data = nii_file.get_fdata()
-#                 png_image = convert_nii_to_png(data)
-#                 images.append(png_image)
-
-#     return jsonify({'images': images})
-
-# def convert_nii_to_png(data):
-#     # Assuming the data is 3D, take the middle slice for visualization
-#     slice_2d = data[:, :, data.shape[2] // 2]
-#     plt.imshow(slice_2d, cmap='gray')
-#     buf = BytesIO()
-#     plt.savefig(buf, format='png')
-#     buf.seek(0)
-#     img_base64 = base64.b64encode(buf.read()).decode('utf-8')
-#     plt.close()
-#     return img_base64
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask, request, jsonify, render_template
 import os
 import gzip
@@ -62,8 +6,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
+# from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
+
+# Load the model
+# model = load_model('static/model/3D-UNet-2018-weights-improvement-01-0.984.hdf5')
 
 @app.route('/')
 def index():
@@ -96,8 +44,25 @@ def upload_files():
 
     return jsonify({'images': images})
 
+@app.route('/analyze', methods=['POST'])
+def analyze_files():
+    files = request.files.getlist('files')
+    results = []
+
+    # for file in files:
+    #     if file.filename.endswith('.gz'):
+    #         with gzip.open(file, 'rb') as f_in:
+    #             nii_file = nib.Nifti1Image.from_bytes(f_in.read())
+    #             data = nii_file.get_fdata()
+    #             # Perform analysis using the model
+    #             prediction = model.predict(np.expand_dims(data, axis=0))
+    #             # Convert prediction to a displayable format
+    #             result_image = convert_nii_to_png(prediction[0])
+    #             results.append(result_image)
+
+    return jsonify({'results': results})
+
 def convert_nii_to_png(data):
-    # Assuming the data is 3D, take the middle slice for visualization
     slice_2d = data[:, :, data.shape[2] // 2]
     plt.imshow(slice_2d, cmap='gray')
     buf = BytesIO()
@@ -108,7 +73,5 @@ def convert_nii_to_png(data):
     return img_base64
 
 if __name__ == '__main__':
-    # Get port from environment variable or default to 10000
     port = int(os.environ.get('PORT', 10000))
-    # Bind to 0.0.0.0 to make the server publicly accessible
     app.run(host='0.0.0.0', port=port, debug=False)
